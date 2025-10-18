e098995c4a4eb64aeb8e3e924488e9ef9286653a
Savi118 <sakshamviraj@amityonline.com>
2025-10-18 18:46:17 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://todo-backend-x1m5.onrender.com/",
 });
 
 api.interceptors.request.use((config) => {
